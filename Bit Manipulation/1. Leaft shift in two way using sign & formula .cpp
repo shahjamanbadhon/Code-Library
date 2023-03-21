564a8d648829b213bccb34e5b4f3e632573d4134
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-03-22 01:12:32 +0600

    Create 1. Leaft shift in two way using sign & formula .cpp

---
+++ Bit Manipulation/1. Leaft shift in two way using sign & formula .cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int x,i,n;
+    cout<<"Enter a Number: ";
+    cin>>x;
+    cout<<"Enter Shift number: ";
+    cin>>i;
+
+    n = x << i;
+    cout<<n<<endl;
+
+
+    // 2nd way: here we can use a formula for operate leaft shift operation { n = x.2^i }
+
+    /*
+    int x,i,n;
+    cout<<"Enter a Number: ";
+    cin>>x;
+    cout<<"Enter Shift number: ";
+    cin>>i;
+
+    int m = pow(2,i);
+    n = x*m;
+    cout<<n<<endl;
+    */
+
+
+
+}
