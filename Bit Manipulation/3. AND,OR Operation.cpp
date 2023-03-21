655543cf82f4360c9e234ce3c94ab2e11f419ae7
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-03-22 01:24:17 +0600

    Add files via upload

---
+++ Bit Manipulation/3. AND,OR Operation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int x,y,a,o;
+    cout<<"Enter Number 1: ";
+    cin>>x;
+    cout<<"Enter Number 2: ";
+    cin>>y;
+
+    a = x & y;
+    o = x | y;
+
+    cout<<"AND: "<<a<<endl;
+    cout<<"OR: "<<o<<endl;
+
+}
