a149a54526765c04a9d3fda256b0fcdc70cd7332
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-03-23 22:59:52 +0600

    Update 3. AND,OR Operation.cpp

--- Bit Manipulation/3. AND,OR Operation.cpp
+++ Bit Manipulation/3. AND,OR Operation.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int x,y,a,o;
+    int x,y,a,o,xor;
     cout<<"Enter Number 1: ";
     cin>>x;
     cout<<"Enter Number 2: ";
@@ -12,8 +12,10 @@ int main()
 
     a = x & y;
     o = x | y;
+    o = x ^ y;
 
     cout<<"AND: "<<a<<endl;
     cout<<"OR: "<<o<<endl;
+    cout<<"XOR: "<<xor<<endl;
 
 }
