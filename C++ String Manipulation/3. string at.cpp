78027c958187c23214289afba6e75d279503caad
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-03-31 14:23:20 +0600

    Add files via upload

---
+++ C++ String Manipulation/3. string at.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s = "new string";
+    cout<<s<<endl;
+
+    //replace i at w
+    s.at(2) = 'i';
+    cout<<s<<endl;
+
+
+
+
+
+}
+
