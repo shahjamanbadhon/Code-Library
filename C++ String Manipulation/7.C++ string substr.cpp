29e0510fb9cf2533806f4acd24d82f720ca28c9b
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-03-31 14:59:18 +0600

    Add files via upload

---
+++ C++ String Manipulation/7.C++ string substr.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string s = "new string";
+
+    cout<<s.substr(4)<<endl;
+    cout<<s.substr(4,3)<<endl;
+}
+
