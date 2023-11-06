28e49e8b04da7b85084b7d41278bf826ef8455bd
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2023-11-07 01:30:05 +0600

    Update LinkedList.cpp

--- Data Structure and Algorithm/DS Implementations/LinkedList.cpp
+++ Data Structure and Algorithm/DS Implementations/LinkedList.cpp
@@ -1,23 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 typedef struct LinkedList *node;
 
-//Define Linked List Structure
+// Define Linked List Structure
 struct LinkedList
 {
     int data;
     struct LinkedList *next;
 };
 
-
-//Create Node
+// Create Node
 node createNode(int value, node n)
 {
     node temp;
-    temp = (node) malloc(sizeof(struct LinkedList));
-    temp -> data = value;
-    temp -> next = n;
+    temp = (node)malloc(sizeof(struct LinkedList));
+    temp->data = value;
+    temp->next = n;
     return temp;
 }
 
@@ -32,54 +31,63 @@ void insertAfter(int key, int value)
 {
     node temp = head;
 
-    while(temp !=NULL && temp->data !=key)
-        temp=temp->next;
+    while (temp != NULL && temp->data != key)
+        temp = temp->next;
 
-    if(temp!=NULL)
+    if (temp != NULL)
         temp->next = createNode(value, temp->next);
-
 }
 
 void insertLast(int value)
 {
+    node temp = head;
 
+    // If the list is empty, insert the node as the first node.
+    if (temp == NULL)
+    {
+        head = createNode(value, NULL);
+        return;
+    }
+
+    // Traverse the list to find the last node.
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    // Insert the new node as the last node.
+    temp->next = createNode(value, NULL);
 }
-
-
 
 void traverse()
 {
     node temp = head;
-    while(temp != NULL)
+    while (temp != NULL)
     {
-        cout<< temp->data <<endl;
+        cout << temp->data << endl;
         temp = temp->next;
     }
 }
 
-
 void deleteNode(int key)
 {
-    node temp;
-    temp = head;
+    node temp = head;
 
-    if(temp -> data == key)
+    if (temp != NULL && temp->data == key)
     {
-        temp = head->next;
-        //free(head);
-        head = temp;
+        head = temp->next;
+        free(temp);
     }
-    else{
-        node prev;
-        while(temp!=NULL && temp->data != key)
+    else
+    {
+        node prev = NULL;
+        while (temp != NULL && temp->data != key)
         {
             prev = temp;
             temp = temp->next;
         }
-        if(temp!=NULL)
+        if (temp != NULL)
         {
             prev->next = temp->next;
-            //free(temp);
+            free(temp);
         }
     }
 }
@@ -89,8 +97,9 @@ int main()
     addFirst(10);
     addFirst(20);
     insertAfter(20, 30);
+    insertLast(40); // Insert a node at the end
     traverse();
-    cout<<endl;
+    cout << endl;
     deleteNode(20);
     traverse();
 
