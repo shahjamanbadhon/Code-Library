35c28f3508c3bfcc30c7dbfc6911521e6c3f46fa
Badhon <107683526+shahjamanbadhon@users.noreply.github.com>
2024-03-31 00:24:26 +0600

    Create sieve optimal.cpp

---
+++ Number Theory/sieve optimal.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+const long long MAX = 1e7+10;
+long long nPrimes;
+long long primes[MAX];
+bool mark[MAX];
+
+void sieve(long long n){
+
+    long long i,j,limit = sqrt(n*1.)+2;
+    mark[1] = true;
+
+    for (long long i = 4; i <= n; i+=2){
+        mark[i] = true;
+    }
+    primes[nPrimes++] = 2;
+
+    for ( i = 3; i <= n; i += 2){
+        if(!mark[i]){
+            primes[nPrimes++] = i;
+            if(i <= limit){
+                for( j = i*i; j <= n; j += i*2){
+                    mark[j] = true;
+                }
+            }
+        }
+    }
+}
+
+bool isPrime(long long n){
+    if(n < 2) return false;
+    if(n == 2 ) return true;
+    if(n % 2) return false;
+    return mark[n] == false;
+
+}
+
+int main(){
+    int n = 100;
+    sieve(n);
+    for(int i = 1; i <= n; ++i){
+        if(mark[i] == false) cout << i <<'\n';
+    }
+
+}
